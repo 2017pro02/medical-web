313cc3e1157ff58ae3eb751bbf762fd5807dd866
i544c <leo@i544c.me>
2017-10-07 19:49:18 +0900

    RailsAdminにアクセス出来るユーザーを制限するようにした

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,7 +3,8 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-    warden.authenticate! scope: :user
+    # warden.authenticate! scope: :user
+    redirect_to main_app.root_path unless current_user&.is_admin?
   end
   config.current_user_method(&:current_user)
 
