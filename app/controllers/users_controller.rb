5e74786eed0403bc99c3a15961ddfad81f34fb4e
i544c <leo@i544c.me>
2017-10-15 00:13:17 +0900

    ユーザーと食事のURLをネストし、ユーザー関係の移行をした

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to user_path(@profile.user.username), notice: "プロフィールを更新しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def set_user
+      @user = UserProfile.find_by!(username: params[:name][1..-1]).user
+      @profile = @user.profile
+    end
+
+    def profile_params
+      params.require(:user_profile).permit(:username, :nickname, :avatar, :bio)
+    end
+end
