43067c847fbaf7d822dc1a4521a208b60a4d9099
i544c <leo@i544c.me>
2017-10-17 18:08:13 +0900

    URL構成変更後のテストに変更

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "GET #show" do
+    subject { get :show, params: params }
+
+    context "存在するnameでアクセスした時" do
+      let(:params) { { name: user.username } }
+      it_behaves_like "ログイン画面に遷移する"
+    end
+
+    context "存在しないnameでアクセスした時" do
+      let(:params) { { name: "@sachiko" } }
+      it_behaves_like "ログイン画面に遷移する"
+    end
+  end
+end
--- spec/routing/meals_routing_spec.rb
+++ spec/routing/meals_routing_spec.rb
@@ -3,35 +3,35 @@ require "rails_helper"
 RSpec.describe MealsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/meals").to route_to("meals#index")
+      expect(get: "/@koume1/meals").to route_to("meals#index", user_name: "@koume1")
     end
 
     it "routes to #new" do
-      expect(get: "/meals/new").to route_to("meals#new")
+      expect(get: "/@koume1/meals/new").to route_to("meals#new", user_name: "@koume1")
     end
 
     it "routes to #show" do
-      expect(get: "/meals/1").to route_to("meals#show", id: "1")
+      expect(get: "/@koume1/meals/2017/03/28").to route_to("meals#show", user_name: "@koume1", date: "2017/03/28")
     end
 
     it "routes to #edit" do
-      expect(get: "/meals/1/edit").to route_to("meals#edit", id: "1")
+      expect(get: "/@koume1/meals/2017/03/28/edit").to route_to("meals#edit", user_name: "@koume1", date: "2017/03/28")
     end
 
     it "routes to #create" do
-      expect(post: "/meals").to route_to("meals#create")
+      expect(post: "/@koume1/meals").to route_to("meals#create", user_name: "@koume1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/meals/1").to route_to("meals#update", id: "1")
+      expect(put: "/@koume1/meals/2017/03/28").to route_to("meals#update", user_name: "@koume1", date: "2017/03/28")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/meals/1").to route_to("meals#update", id: "1")
+      expect(patch: "/@koume1/meals/2017/03/28").to route_to("meals#update", user_name: "@koume1", date: "2017/03/28")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/meals/1").to route_to("meals#destroy", id: "1")
+      expect(delete: "/@koume1/meals/2017/03/28").to route_to("meals#destroy", user_name: "@koume1", date: "2017/03/28")
     end
   end
 end
