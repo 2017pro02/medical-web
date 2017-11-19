c991e947df28fcb79fb9e0536524e6ae1319a685
i544c <leo@i544c.me>
2017-11-19 22:23:54 +0900

    栄養素のアクセスにはログイン必須とした

--- app/controllers/nutritions_controller.rb
+++ app/controllers/nutritions_controller.rb
@@ -1,4 +1,5 @@
 class NutritionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_nutrition, only: [:show, :edit, :update, :destroy]
 
   # GET /nutritions
