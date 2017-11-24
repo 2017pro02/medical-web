85376604744eed76b081132f0c3a79c112471cb4
i544c <leo@i544c.me>
2017-11-24 17:29:18 +0900

    ダッシュボードで最近の料理画像も表示するようにした
    ついでに更新時間のバグも修正した。

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,7 +1,7 @@
 Time::DATE_FORMATS[:human] = lambda {|date|
-  seconds = Time.now.change(hour:0) - date;
-  days    = (seconds / (60 * 60 * 24)).ceil; return "#{days}日前" if days > 0
-  hours   = (seconds / (60 * 60)).ceil; return "#{hours}時間前" if hours > 0
-  minutes = (seconds / 60).ceil; return "#{minutes}分前" if minutes > 0
+  seconds = (Time.now - date).round;
+  days    = seconds / (60 * 60 * 24); return "#{days}日前" if days > 0
+  hours   = seconds / (60 * 60); return "#{hours}時間前" if hours > 0
+  minutes = seconds / 60; return "#{minutes}分前" if minutes > 0
   return "#{seconds}秒前"
 }
