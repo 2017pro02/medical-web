d9ebe32c3e20280af200f5a1877612f1e8e99a3c
i544c <leo@i544c.me>
2017-12-01 12:09:59 +0900

    栄養素のシードを追加

---
+++ db/fixtures/03_nutrition.rb
@@ -0,0 +1,25 @@
+require "json"
+
+json_path = Rails.root.join("db", "fixtures", "nutritions.json").to_path
+data = open(json_path) do |io|
+  JSON.load(io)
+end
+
+def seikika(str)
+  return if str.blank?
+  str.gsub(/[a-z]*(\s*\(.*\))?/, "").to_f
+end
+
+data.each_with_index do |nu, i|
+  Nutrition.seed do |n|
+    n.id = i
+    n.name = nu["name"]
+    nut = nu["nutritions"]
+    n.energy = nut["エネルギー"]
+    n.protein = seikika(nut["タンパク質"])
+    n.lipid = seikika(nut["脂質"])
+    n.carbohydrate = seikika(nut["炭水化物"])
+    n.dietary_fiber = seikika(nut["食物繊維 総量"])
+    n.saturated_fatty_acid = seikika(nut["脂肪酸 飽和"])
+  end
+end
