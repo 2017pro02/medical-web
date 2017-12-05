b94fd62d3c4bcd969d715f7566c74ea8fbfd5147
i544c <leo@i544c.me>
2017-12-06 03:59:43 +0900

    テレビに過去のコメントが表示されるようにした

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
     @meals2 = Meal.where(user: current_user, created_at: (Date.current - 2).all_day)
     @meals1 = Meal.where(user: current_user, created_at: (Date.current - 1).all_day)
     @meals0 = Meal.where(user: current_user, created_at: Date.current.all_day)
-    @world = "world"
+    @comments = Comment.where(target_user: current_user, target_date: (Date.current - 2)...Date.current + 1)
     render layout: false
   end
 end
--- app/views/pages/tv.json.jbuilder
+++ app/views/pages/tv.json.jbuilder
@@ -10,4 +10,8 @@ json.zero do
   json.array! @meals0, partial: "meals/meal", as: :meal
 end
 
-json.set! :hello, @world
+json.comments do
+  json.array! @comments do |comment|
+    json.extract! comment, :id, :target_user, :message
+  end
+end
